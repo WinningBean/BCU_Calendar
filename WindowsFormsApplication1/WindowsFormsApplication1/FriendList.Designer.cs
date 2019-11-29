da2870be0747286bc05cfa8687f3e75ddad80561
jueun275 <52851721+jueun275@users.noreply.github.com>
2019-11-29 10:39:53 +0900

    making friendList form
    Label 동적 생성 , 폼길이 넘어갈시 스크롤 가능

--- WindowsFormsApplication1/WindowsFormsApplication1/FriendList.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/FriendList.Designer.cs
@@ -28,22 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DBGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.DBGrid2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 529);
+            this.panel1.TabIndex = 1;
+            // 
+            // DBGrid
+            // 
+            this.DBGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DBGrid.Location = new System.Drawing.Point(388, 31);
+            this.DBGrid.Name = "DBGrid";
+            this.DBGrid.RowTemplate.Height = 27;
+            this.DBGrid.Size = new System.Drawing.Size(336, 157);
+            this.DBGrid.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "친구친청";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // DBGrid2
+            // 
+            this.DBGrid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DBGrid2.Location = new System.Drawing.Point(388, 219);
+            this.DBGrid2.Name = "DBGrid2";
+            this.DBGrid2.RowTemplate.Height = 27;
+            this.DBGrid2.Size = new System.Drawing.Size(336, 157);
+            this.DBGrid2.TabIndex = 4;
             // 
             // FriendList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(249, 606);
+            this.ClientSize = new System.Drawing.Size(756, 606);
+            this.Controls.Add(this.DBGrid2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DBGrid);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FriendList";
             this.Text = "FriendList";
             this.Load += new System.EventHandler(this.FriendList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView DBGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView DBGrid2;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/FriendList.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/FriendList.cs
@@ -8,18 +8,97 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WindowsFormsApplication1
 {
     public partial class FriendList : Form
     {
+
+       // int count = 0;
+        
+        DBConnection db = Program.DB;
+        DataTable friendTable = null;
+        int location;
+
+
         public FriendList()
         {
             InitializeComponent();
+            int location = 0;
+        }
+        
+        private void UploadFriendList()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                ComboBox com = new ComboBox();
+                com.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+                com.Name = "com"+i;
+                com.Location = new System.Drawing.Point(50, location + i * 30);
+                com.Text = "친구목록";
+
+            }
+        }
+        
+        private void FriendGroup_Click(object sender)
+        {
+
+        }
+
+        private void UploadeList(int location)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Label label = new Label();
+                label.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+                label.Location = new System.Drawing.Point(110, location + i * 30);
+                label.Name = "label" + i.ToString();
+                label.Size = new System.Drawing.Size(120, 23);
+                label.TabIndex = i;
+                label.Text = "최주은" + i;
+                panel1.Controls.Add(label);
+
+            }
+        }
+        private void GetFriendsList()
+        {
+            db.UR_CD = "U100001";
+            db.AdapterOpen("select fr_fr_fk from friend_tb WHERE fr_ur_fk = '" + db.UR_CD + "' ORDER BY fr_fr_fk DESC");
+
+            DataSet DS = new DataSet();
+            db.Adapter.Fill(DS, "friend_tb");
+            friendTable = DS.Tables["friend_tb"];
+            DBGrid.DataSource = DS.Tables["friend_tb"].DefaultView;
+
+            int count = friendTable.Rows.Count;
+
+            //location = 50 + count * 30;
+
+            db.Close();
         }
 
         private void FriendList_Load(object sender, EventArgs e)
         {
+            panel1.HorizontalScroll.Maximum = 0;
+            panel1.VerticalScroll.Maximum = 0;
+            panel1.AutoScroll = false;
+            panel1.VerticalScroll.Visible = false;
+            panel1.AutoScroll = true;
+        
+            GetFriendsList();
+            UploadeList(location);
+
 
         }
+
+        private void GetGroupList()
+        {
+            db.AdapterOpen("select fr_fr_fk from friend_tb WHERE fr_ur_fk = '" + db.UR_CD + "' ORDER BY fr_fr_fk DESC");
+
+            DataSet ds = new DataSet();
+
+        }
+
     }
-}
+    }
+
