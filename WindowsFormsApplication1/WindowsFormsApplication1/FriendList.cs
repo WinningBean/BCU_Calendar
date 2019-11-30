98c3ff8b2a782ac15fd94e56ca711a6813384619
jueun275 <52851721+jueun275@users.noreply.github.com>
2019-11-30 15:42:16 +0900

    making friend list
    sql 변경 + 뻘짓??

--- WindowsFormsApplication1/WindowsFormsApplication1/FriendList.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/FriendList.cs
@@ -13,16 +13,18 @@ namespace WindowsFormsApplication1
 {
     public partial class FriendList : Form
     {
-        
 
-       // int count = 0;
+
+        // int count = 0;
 
         DBConnection db = Program.DB;
         DataTable friendTable = null;
         DataTable friend_group_tb = null;
-        ListBox[] list ;
+        ListBox[] list;
         Label[] label;
         Button[] btn;
+
+        DataSet groupMember = new DataSet();
 
         int location;
         int[] flag;
@@ -34,20 +36,20 @@ namespace WindowsFormsApplication1
             InitializeComponent();
             location = 0;
         }
-        
+
         private void UploadFriendList()
         {
             for (int i = 0; i < 3; i++)
             {
                 ComboBox com = new ComboBox();
                 com.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-                com.Name = "com"+i;
+                com.Name = "com" + i;
                 com.Location = new System.Drawing.Point(50, location + i * 30);
                 com.Text = "친구목록";
 
             }
         }
-        
+
         private void FriendGroup_Click(object sender)
         {
 
@@ -55,7 +57,7 @@ namespace WindowsFormsApplication1
 
         private void UploadeList(int location)
         {
-            flag = new int[friendTable.Rows.Count+1];
+            flag = new int[friendTable.Rows.Count + 1];
             Label[] label = new Label[friendTable.Rows.Count];
             Button[] btn = new Button[friend_group_tb.Rows.Count];
 
@@ -69,7 +71,7 @@ namespace WindowsFormsApplication1
             panel1.Controls.Add(btnbase);
             flag[0] = 1;
 
-            for ( int i = 0; i < friendTable.Rows.Count; i++)
+            for (int i = 0; i < friendTable.Rows.Count; i++)
             {
                 DataRow currRow = friendTable.Rows[i];
                 label[i] = new Label();
@@ -78,7 +80,7 @@ namespace WindowsFormsApplication1
                 label[i].Name = "label" + i.ToString();
                 label[i].Size = new System.Drawing.Size(100, 25);
                 label[i].TabIndex = i;
-                label[i].Text = currRow["fr_fr_fk"].ToString();
+                label[i].Text = currRow["UR_NM"].ToString();
                 panel1.Controls.Add(label[i]);
                 location++;
             }
@@ -94,6 +96,7 @@ namespace WindowsFormsApplication1
                 btn[i].Size = new System.Drawing.Size(120, 32);
                 btn[i].TabIndex = i;
                 btn[i].Text = currRow["FRGR_NM"].ToString() + "       ▼";
+               // btn[i].Click +=
                 panel1.Controls.Add(btn[i]);
 
                 location++;
@@ -105,14 +108,14 @@ namespace WindowsFormsApplication1
         private void GetFriendsList()
         {
             db.UR_CD = "U100000";
-            db.AdapterOpen("select fr_fr_fk, FR_FRGR_FK from friend_tb WHERE fr_ur_fk = '" + db.UR_CD + "' ORDER BY fr_fr_fk DESC");
+            db.AdapterOpen("select  UR_NM  from USER_TB  WHERE ur_cd  in (select FR_FR_FK from FRIEND_TB where FR_UR_FK = '" + db.UR_CD + "') ORDER BY  UR_NM ASC");
 
             DataSet DS = new DataSet();
             db.Adapter.Fill(DS, "friend_tb");
             friendTable = DS.Tables["friend_tb"];
             DBGrid.DataSource = DS.Tables["friend_tb"].DefaultView;
 
-            int count = friendTable.Rows.Count;
+           // int count = friendTable.Rows.Count;
 
             //location = 50 + count * 30;
 
@@ -123,17 +126,17 @@ namespace WindowsFormsApplication1
         {
             panel1.HorizontalScroll.Maximum = 0;
             panel1.VerticalScroll.Maximum = 0;
-         //   panel1.AutoScroll = false;
+            //   panel1.AutoScroll = false;
             panel1.VerticalScroll.Visible = false;
             panel1.AutoScroll = true;
-        
+
             GetFriendsList();
             GetGroupList();
             UploadeList(location);
 
-          // GetGroupMamber();
+            // GetGroupMamber();
             listBox1.BorderStyle = BorderStyle.None;
-           //listBox1. = false;
+            //listBox1. = false;
             //listBox1.AutoSize = True;
             listBox1.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             //listBox1.autosize
@@ -144,14 +147,15 @@ namespace WindowsFormsApplication1
             db.AdapterOpen("select FRGR_CD, FRGR_NM from FRIEND_GROUP_TB WHERE FRGR_UR_FK = '" + db.UR_CD + "' ORDER BY FRGR_NM DESC");
 
             DataSet ds = new DataSet();
+            
             db.Adapter.Fill(ds, "friend_group_tb");
             friend_group_tb = ds.Tables["friend_group_tb"];
             DBGrid2.DataSource = ds.Tables["friend_group_tb"].DefaultView;
-            
+
             for (int i = 0; i < friend_group_tb.Rows.Count; i++)
             {
                 DataRow currRow = friend_group_tb.Rows[i];
-                
+
 
             }
 
@@ -159,10 +163,10 @@ namespace WindowsFormsApplication1
         }
         private void GetGroupMamber()
         {
-            
-            for(int i = 0; i < friend_group_tb.Rows.Count; i++)
+
+            for (int i = 0; i < friend_group_tb.Rows.Count; i++)
             {
-                
+
                 int count = 0;
                 DataRow GcurrRow = friend_group_tb.Rows[i];
                 for (int j = 0; j < friendTable.Rows.Count; j++)
@@ -175,14 +179,14 @@ namespace WindowsFormsApplication1
                     panel1.Controls.Add(list[i]);
 
 
-                    if ((GcurrRow["FRGR_CD"].ToString()).Equals( McurrRow["FR_FRGR_FK"].ToString()))
+                    if ((GcurrRow["FRGR_CD"].ToString()).Equals(McurrRow["FR_FRGR_FK"].ToString()))
                     {
                         list[i].Items.Add(McurrRow["FR_FRGR_FK"].ToString());
                         count++;
                     }
-                   
+
                 }
-               
+
 
             }
         }
@@ -192,7 +196,7 @@ namespace WindowsFormsApplication1
 
             if (flag[i] == 1)
             {
-                list[i].Visible = true; 
+                list[i].Visible = true;
                 for (int j = i; j < 0; j--)
                 {
                     // btn[i].Location .Y += 30 
@@ -204,17 +208,38 @@ namespace WindowsFormsApplication1
             else
             {
 
-                for(int j = 0; j > i; j--)
+                for (int j = 0; j > i; j--)
                 {
 
                 }
             }
         }
 
+        private void GroupListBottunClick(object sender , EventArgs e)
+        {
+            Button mybtn = (Button)sender;
+            string str = mybtn.Name;
 
-       private void 
+            for(int i = 0; i < friendTable.Rows.Count; i++)
+            {
+                DataRow currRow = friendTable.Rows[i];
+                if (currRow["FRGR_NM"].ToString().Equals(str))
+                {
 
-        
-    }
+                }
+            }
+            
+
+
+
+
+
+
+
+        }
+
+
+
+}
     }
 
