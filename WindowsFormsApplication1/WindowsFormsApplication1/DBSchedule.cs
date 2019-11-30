41411f7dbd1d9e5bfd040dda052618316fa8e50d
wsb0722 <winningbean@gmail.com>
2019-11-30 22:20:02 +0900

    UPDATE DBSchedule.cs
    - UPDATE UPDATE DBSchedule's FUNCTION for GROUP
    - ADD Get_Schedule() FUNCTION and REMOVE Get_User_Schedule() FUNTION
    - UPDATE Get_Day_Schedule() FUNCTION

--- WindowsFormsApplication1/WindowsFormsApplication1/DBSchedule.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/DBSchedule.cs
@@ -18,36 +18,51 @@ namespace WindowsFormsApplication1
         DBConnection db = Program.DB;
         private string sql;
         private DataSet DS = new DataSet();
-        private DataTable USER_SC_TB = new DataTable();
+
         // DB상 Schedule 가져오기 클래스
 
-        public DataTable Get_User_Schedule(string m_UR_CD)
+        public DataTable Get_Schedule(string UrOrGr, string m_URorGR_CD)
         {
             // 해당 사용자에 대한 모든 일정 테이블 함수
-            sql = "select * from SCHEDULE_TB where SC_UR_FK = '" + m_UR_CD + "'";
+            if (UrOrGr == "U") // 회원이라면
+            {
+                sql = "select * from SCHEDULE_TB where SC_UR_FK = '" + m_URorGR_CD + "'";
+            }
+            else if(UrOrGr == "G") // 그룹이라면
+            {
+                sql = "select * from SCHEDULE_TB where SC_GR_FK = '" + m_URorGR_CD + "'";
+            }
             
-            //db.Command.CommandText = sql;
             db.AdapterOpen(sql);
-            db.Adapter.Fill(DS, "USER_SC_TB");
+            db.Adapter.Fill(DS, "GET_SC_TB");
 
-            USER_SC_TB = DS.Tables["USER_SC_TB"];
+            DataTable GET_SC_TB = new DataTable();
+            GET_SC_TB = DS.Tables["GET_SC_TB"];
 
-            return USER_SC_TB;
+            return GET_SC_TB;
         }
-
-        public DataTable Get_Day_Schedule(string m_UR_CD, DateTime day) {
+        
+        public DataTable Get_Day_Schedule(string UrOrGr, string m_URorGR_CD, DateTime day)
+        {
             // 해당 날짜에 대한 해당 사용자의 일정 테이블 함수
-            sql = "select * from SCHEDULE_TB where SC_UR_FK = '" + m_UR_CD + "'";
+            if (UrOrGr == "U") // 회원이라면
+            {
+                sql = "select * from SCHEDULE_TB where SC_UR_FK = '" + m_URorGR_CD + "'";
+            }
+            else if (UrOrGr == "G") // 그룹이라면
+            {
+                sql = "select * from SCHEDULE_TB where SC_GR_FK = '" + m_URorGR_CD + "'";
+            }
             sql += " and SC_STR_DT >= '" + day.ToString("yyyy-MM-dd") + "'";
             sql += " and SC_STR_DT < '" + day.AddDays(1).ToString("yyyy-MM-dd") + "'";
-
-            //db.Command.CommandText = sql;
+            
             db.AdapterOpen(sql);
-            db.Adapter.Fill(DS, "USER_SC_DAY_TB");
+            db.Adapter.Fill(DS, "GET_DAY_SC_TB");
 
-            USER_SC_TB = DS.Tables["USER_SC_DAY_TB"];
+            DataTable GET_DAY_SC_TB = new DataTable();
+            GET_DAY_SC_TB = DS.Tables["GET_DAY_SC_TB"];
 
-            return USER_SC_TB;
+            return GET_DAY_SC_TB;
         }
     }
 }
