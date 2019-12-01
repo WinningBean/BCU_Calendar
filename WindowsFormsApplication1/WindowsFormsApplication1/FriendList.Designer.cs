25082202cc149819b31843e5effd9ee48d029765
jueun275 <52851721+jueun275@users.noreply.github.com>
2019-12-02 02:07:49 +0900

    Complete Dynamic Location Event Add Friend Search Function

--- WindowsFormsApplication1/WindowsFormsApplication1/FriendList.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/FriendList.Designer.cs
@@ -29,16 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.DBGrid = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.DBGrid2 = new System.Windows.Forms.DataGridView();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.DBGrid2)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -47,15 +45,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(250, 529);
             this.panel1.TabIndex = 1;
-            // 
-            // DBGrid
-            // 
-            this.DBGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DBGrid.Location = new System.Drawing.Point(256, 77);
-            this.DBGrid.Name = "DBGrid";
-            this.DBGrid.RowTemplate.Height = 27;
-            this.DBGrid.Size = new System.Drawing.Size(336, 157);
-            this.DBGrid.TabIndex = 2;
             // 
             // button1
             // 
@@ -74,67 +63,70 @@
             this.textBox1.Size = new System.Drawing.Size(133, 25);
             this.textBox1.TabIndex = 3;
             // 
-            // DBGrid2
+            // panel2
             // 
-            this.DBGrid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DBGrid2.Location = new System.Drawing.Point(256, 240);
-            this.DBGrid2.Name = "DBGrid2";
-            this.DBGrid2.RowTemplate.Height = 27;
-            this.DBGrid2.Size = new System.Drawing.Size(336, 157);
-            this.DBGrid2.TabIndex = 4;
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(281, 23);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(289, 149);
+            this.panel2.TabIndex = 6;
+            this.panel2.Visible = false;
             // 
-            // groupBox1
+            // button4
             // 
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Location = new System.Drawing.Point(256, 403);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(209, 127);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.button4.Location = new System.Drawing.Point(168, 103);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 26);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "취소";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button2
+            // button3
             // 
-            this.button2.Location = new System.Drawing.Point(27, 39);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "친구목록";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(47, 103);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 26);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "확인";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // label1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Items.AddRange(new object[] {
-            "최주은",
-            "최주은2",
-            "최주은3"});
-            this.listBox1.Location = new System.Drawing.Point(571, 410);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(150, 124);
-            this.listBox1.TabIndex = 1;
+            this.label1.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(-5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 88);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "검색하신 이름의 친구가 없습니다 친구를 추가 하시겠습니까?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSize = true;
+            this.panel3.Location = new System.Drawing.Point(281, 185);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(212, 166);
+            this.panel3.TabIndex = 7;
             // 
             // FriendList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(756, 606);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.DBGrid2);
+            this.ClientSize = new System.Drawing.Size(637, 606);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.DBGrid);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FriendList";
             this.Text = "FriendList";
             this.Load += new System.EventHandler(this.FriendList_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.DBGrid2)).EndInit();
-            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,12 +134,12 @@
 
         #endregion
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView DBGrid;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView DBGrid2;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/FriendList.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/FriendList.cs
@@ -30,24 +30,25 @@ namespace WindowsFormsApplication1
             location = 0;
         }
 
-        private void UploadFriendList()
+        private Control Create_FriendProfile(int i, DataTable dataTable)
         {
-            for (int i = 0; i < 3; i++)
-            {
-                ComboBox com = new ComboBox();
-                com.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-                com.Name = "com" + i;
-                com.Location = new System.Drawing.Point(50, location + i * 30);
-                com.Text = "친구목록";
+            DataRow currRow = dataTable.Rows[i];
+            UserCustomControl.Profile FriendProfile = new UserCustomControl.Profile();          
+            FriendProfile.Set_Profile_Size(30, FontStyle.Bold);
+            FriendProfile.USERNAME = currRow["UR_NM"].ToString();
+            FriendProfile.Location = new System.Drawing.Point(0, 10 + location * 30);
+            FriendProfile.Size = new System.Drawing.Size(150, 25);
 
-            }
+            location++;
+
+            return FriendProfile;
         }
 
-        private void UploadeList(int location)
+        private void UploadeList()
         {
             flag = new int[friendTable.Rows.Count + 1];
             label = new Label[friendTable.Rows.Count];
-            btn = new Button[friend_group_tb.Rows.Count+1];
+            btn = new Button[friend_group_tb.Rows.Count + 1];
             pan = new Panel[friend_group_tb.Rows.Count + 1];
 
             //기본적으로 생성해주는 모든친구 버튼과 목록이 저장되는 판넬 생성 
@@ -55,52 +56,37 @@ namespace WindowsFormsApplication1
             btn[0].Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             btn[0].Location = new System.Drawing.Point(50, 10);
             btn[0].Name = "btn";
+            btn[0].TabIndex = 0;
             btn[0].Size = new System.Drawing.Size(130, 32);
             btn[0].Text = "기본그룹    ▲";
+            btn[0].Click += new EventHandler(GroupList_Click);
             panel1.Controls.Add(btn[0]);
             flag[0] = 1;
 
             pan[0] = new Panel();
             pan[0].AutoSize = true;
             pan[0].Location = new Point(btn[0].Location.X, btn[0].Location.Y + 30);
+           
             panel1.Controls.Add(pan[0]);
 
             for (int i = 0; i < friendTable.Rows.Count; i++)
-            {
-                DataRow currRow = friendTable.Rows[i];
-                UserCustomControl.Profile FriendProfile = new UserCustomControl.Profile();
-                pan[0].Controls.Add(FriendProfile);
-                FriendProfile.Set_Profile_Size(30, FontStyle.Bold);
-                FriendProfile.USERNAME = currRow["UR_NM"].ToString();
-                FriendProfile.Location = new System.Drawing.Point(0, 10 + location * 30);
-                FriendProfile.Size = new System.Drawing.Size(150, 25);
-                
-                location++;
-
-                //label[i] = new Label();
-                //label[i].Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-                //label[i].Location = new System.Drawing.Point(110, 45 + location * 30);
-                //label[i].Name = "label" + i.ToString();
-                //label[i].Size = new System.Drawing.Size(100, 25);
-                //label[i].TabIndex = i;
-                //label[i].Text = currRow["UR_NM"].ToString();
-                //panel1.Controls.Add(label[i]);
-                //location++;
+            {             
+                pan[0].Controls.Add(Create_FriendProfile(i , friendTable));
             }
 
             for (int i = 0; i < friend_group_tb.Rows.Count; i++) // ***CreateGroupButton 함수로 뺄꺼
             {
                 DataRow currRow;
                 currRow = friend_group_tb.Rows[i];
-                btn[i+1] = new Button();
-                btn[i+1].Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-                btn[i+1].Location = new System.Drawing.Point(50, 45 + location * 30);
-                btn[i+1].Name = "btn" + i.ToString();
-                btn[i+1].Size = new System.Drawing.Size(130, 32);
-                btn[i+1].TabIndex = i;
-                btn[i+1].Text = currRow["FRGR_NM"].ToString() + "       ▼";
-                // btn[i+1].Click +=
-                panel1.Controls.Add(btn[i+1]);
+                btn[i + 1] = new Button();
+                btn[i + 1].Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+                btn[i + 1].Location = new System.Drawing.Point(50, 45 + location * 30);
+                btn[i + 1].Name = "btn" + i.ToString();
+                btn[i + 1].Size = new System.Drawing.Size(130, 32);
+                btn[i + 1].TabIndex = i+1;
+                btn[i + 1].Text = currRow["FRGR_NM"].ToString() + "       ▼";
+                btn[i + 1].Click += new EventHandler(GroupList_Click);
+                panel1.Controls.Add(btn[i + 1]);
 
                 location++;
                 flag[i + 1] = 0;
@@ -115,11 +101,6 @@ namespace WindowsFormsApplication1
             DataSet DS = new DataSet();
             db.Adapter.Fill(DS, "friend_tb");
             friendTable = DS.Tables["friend_tb"];
-            DBGrid.DataSource = DS.Tables["friend_tb"].DefaultView;
-
-            // int count = friendTable.Rows.Count;
-
-            //location = 50 + count * 30;
 
             db.Close();
         }
@@ -128,17 +109,13 @@ namespace WindowsFormsApplication1
         {
             panel1.HorizontalScroll.Maximum = 0;
             panel1.VerticalScroll.Maximum = 0;
-            // panel1.AutoScroll = false;
             panel1.VerticalScroll.Visible = false;
             panel1.AutoScroll = true;  //스크롤 
 
             GetFriendsList();
             GetGroupList();
-            UploadeList(location);
-           // getFriendTB();
+            UploadeList();
             GetGroupMamber();
-            // userTB();    
-
         }
 
         private void GetGroupList()
@@ -147,23 +124,19 @@ namespace WindowsFormsApplication1
             DataSet ds = new DataSet();
             db.Adapter.Fill(ds, "friend_group_tb");
             friend_group_tb = ds.Tables["friend_group_tb"];
-            DBGrid2.DataSource = ds.Tables["friend_group_tb"].DefaultView;
-
         }
 
         private void GetGroupMamber() // 그룹 멤버 반환 
         {
-            
+
             for (int i = 0; i < friend_group_tb.Rows.Count; i++)  //panal 생성구간 create panel 로뺄까...?
             {
-
-                int count = 0;
-                pan[i+1] = new Panel();
-                panel1.Controls.Add(pan[i+1]);
-                pan[i+1].Location = new System.Drawing.Point(btn[i+1].Location.X, btn[i+1].Location.Y + 32);
-                pan[i+1].AutoSize = true;
-                pan[i+1].BorderStyle = BorderStyle.FixedSingle;
-
+                location = 0;
+                pan[i + 1] = new Panel();
+                panel1.Controls.Add(pan[i + 1]);
+                pan[i + 1].Location = new System.Drawing.Point(btn[i + 1].Location.X, btn[i + 1].Location.Y + 32);
+                pan[i + 1].AutoSize = true;
+                pan[i + 1].Visible = false;
                 DataRow currRow = friend_group_tb.Rows[i];
                 db.AdapterOpen("select  UR_NM from USER_TB  where UR_CD in (select FR_FR_FK from FRIEND_TB where FR_FRGR_FK = '" + currRow["FRGR_CD"].ToString() + "') ORDER BY  UR_NM ASC");
 
@@ -173,78 +146,66 @@ namespace WindowsFormsApplication1
 
                 for (int j = 0; j < groupMemberTb.Rows.Count; j++)
                 {
-                    DataRow McurrRow = groupMemberTb.Rows[j];
-                    UserCustomControl.Profile FriendProfile = new UserCustomControl.Profile();
-                    FriendProfile.Set_Profile_Size(30, FontStyle.Bold);
-                    FriendProfile.USERNAME = McurrRow["UR_NM"].ToString();
-                    FriendProfile.Size = new System.Drawing.Size(150, 25);
-                    pan[i + 1].Controls.Add(FriendProfile);
-                    FriendProfile.Location = new Point(0, count * 30);
-
-                    //Label li = new Label();
-                    //li.Size = new System.Drawing.Size(100, 25);
-                    //li.Text = McurrRow["UR_NM"].ToString();
-                    //li.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-                    //pan[i+1].Controls.Add(li);
-                    //li.Location = new Point(0, count * 30);
-                    listBox1.Items.Add(McurrRow["UR_NM"].ToString()); 
-
-                    count++;
-
+                    pan[i + 1].Controls.Add( Create_FriendProfile(j, groupMemberTb));
                 }
             }
         }
 
-        //private void getFriendTB()
-        //{
-        //    db.AdapterOpen("select FR_UR_FK, FR_FR_FK ,FR_FRGR_FK from FRIEND_TB WHERE FR_UR_FK = '" + db.UR_CD + "' ");
-
-        //    DataSet ds = new DataSet();
-
-        //    db.Adapter.Fill(ds, "friendtb");
-        //    friendtb = ds.Tables["friendtb"];
-
-        //}
-        //private void userTB()
-        //{
-        //    db.AdapterOpen("select UR_CD, UR_NM  from USER_TB WHERE UR_CD = '" + db.UR_CD + "' ORDER BY UR_NM ASC");
-
-        //    DataSet ds = new DataSet();
-
-        //    db.Adapter.Fill(ds, "usertb");
-        //    usertb = ds.Tables["usertb"];
-
-        //}
-
-        private void GroupList_Click(object render, EventArgs e)  // 동적생성 버튼의 이벤드 아직 연결 안시킴 
+        private void GroupList_Click(object render, EventArgs e)
         {
-            int i = 0; // 눌린 버튼이 몇째버튼인지 구해서 i 에  넣어줘야함
-                if (flag[i] == 0)
+            int i = 0;
+            Button mybtn = (Button)render;
+            i = mybtn.TabIndex ;
+       
+            if (!pan[i].Visible)
+            {
+                for (int j = i +1 ; j < friend_group_tb.Rows.Count + 1; j++)
                 {
-                    pan[i].Visible = true;
-                    for (int j = i; j < friend_group_tb.Rows.Count+1; j++)
-                    {
-                        btn[j].Location = new Point(btn[j].Location.X, btn[j].Location.Y + pan[j].Size.Height);
-                        pan[j].Location = new System.Drawing.Point(btn[j].Location.X, btn[j].Location.Y + 32);
-                    }
+                    btn[j].Location = new Point(btn[j].Location.X, btn[j].Location.Y + pan[i].Size.Height);
+                    pan[j].Location = new System.Drawing.Point(btn[j].Location.X, btn[j].Location.Y + 32);
                 }
-                else
+                pan[i].Visible = true;
+            }
+            else
+            {
+                for (int j = i+1; j < friend_group_tb.Rows.Count + 1; j++)
                 {
-                    pan[i].Visible = false;
-                    for (int j = i; j < friend_group_tb.Rows.Count+1; j++)
-                    {
-                        btn[j].Location = new Point(btn[j].Location.X, btn[j].Location.Y - pan[j].Size.Height);
-                        pan[j].Location = new System.Drawing.Point(btn[j].Location.X, btn[j].Location.Y + 32);
-                    }
-
-                } 
+                    btn[j].Location = new Point(btn[j].Location.X, btn[j].Location.Y - pan[i].Size.Height);
+                    pan[j].Location = new System.Drawing.Point(btn[j].Location.X, btn[j].Location.Y + 32);
+                }
+                pan[i].Visible = false;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            location = 0;
+            panel3.Controls.Clear();
+            bool check = false;
+            for (int i = 0; i < friendTable.Rows.Count; i++)
+            {
+                DataRow currRow = friendTable.Rows[i];
+                if (currRow["UR_NM"].ToString().Equals(textBox1.Text))
+                {
+                    check = true;
+                    panel3.Controls.Add( Create_FriendProfile(i, friendTable));                   
+                }       
+            }
+            if(!check)
+            {
+                panel1.Visible = false;
+                panel2.Visible = true;
+            }
+            textBox1.Text = "";        
+        }
 
+        private void button4_Click(object sender, EventArgs e) // 취소
+        {
+            panel2.Visible = false;
+            panel1.Visible = true;
         }
     }
-
 }
+
+
 
