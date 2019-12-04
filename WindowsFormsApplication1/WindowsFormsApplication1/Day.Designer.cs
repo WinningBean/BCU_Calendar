c5382e92f0d7eb687ba4584962f208fd5f597c06
jueun275 <52851721+jueun275@users.noreply.github.com>
2019-12-04 14:15:07 +0900

    Day Form Design
    -일간 폼 디자인

--- WindowsFormsApplication1/WindowsFormsApplication1/Day.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Day.Designer.cs
@@ -28,21 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.확인 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("함초롬돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(8, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "2019";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(2, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 470);
+            this.panel1.TabIndex = 3;
+            // 
+            // 확인
+            // 
+            this.확인.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.확인.Location = new System.Drawing.Point(787, 23);
+            this.확인.Name = "확인";
+            this.확인.Size = new System.Drawing.Size(104, 33);
+            this.확인.TabIndex = 4;
+            this.확인.Text = "확인";
+            this.확인.UseVisualStyleBackColor = true;
+            this.확인.Click += new System.EventHandler(this.확인_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(211, 63);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(704, 470);
+            this.panel2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("함초롬돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(70, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "12";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("함초롬돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(117, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "25";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("함초롬돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(164, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "수";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("함초롬돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(38, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "오늘의 할일";
             // 
             // Day
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(610, 495);
+            this.ClientSize = new System.Drawing.Size(916, 533);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.확인);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Day";
             this.Text = "Day";
+            this.Load += new System.EventHandler(this.Day_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button 확인;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/Day.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Day.cs
@@ -12,9 +12,121 @@ namespace WindowsFormsApplication1
 {
     public partial class Day : Form
     {
+        DBConnection db = Program.DB;
+        Panel paintPan = new Panel();
+
+        DataTable pictureTb = null;
+        List<Panel> time = null;
+
+        private int m_thisYear;
+        private int m_thisMonth; 
+        private int m_thisDay; 
+        private int m_thisDayOfWeek; // Day From 이 열릴떄 Month에서 받아옴 
+
         public Day()
         {
             InitializeComponent();
+            time = new List<Panel>();
+        }
+
+        
+        private void Draw_Line(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            int x = 10;
+            int y = 60;
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(Color.Black);
+
+            for (int i = 0; i < 25; i++)
+            {
+                Panel pan = new Panel();
+                graphics.DrawLine(pen, x, 0, x, 60);
+                x += 150;
+            }
+        }
+
+        private void Day_Load(object sender, EventArgs e)
+        {
+            
+            paintPan.Location = new Point(0, 0);
+            paintPan.Size = new Size(3640, 355);
+            paintPan.Show();
+            paintPan.Paint += new System.Windows.Forms.PaintEventHandler(Draw_Line);
+            // paintPan.BackColor = Color.Transparent;
+            panel2.Controls.Add(paintPan);
+
+            Draw_Time();
+            panel2.VerticalScroll.Maximum = 0;
+            panel2.HorizontalScroll.Maximum = 0;
+            panel2.VerticalScroll.Visible = false;
+            panel2.AutoScroll = true;
+
+            //panel2.Paint += new System.Windows.Forms.PaintEventHandler(Draw_Line);
+
+        }
+        private void Draw_Time()
+        {
+           
+                int x = 1;
+            for (int i = 0; i < 25; i++)
+            {
+                Label timeLable = new Label();
+                timeLable.Text = (i).ToString();
+
+
+                timeLable.AutoSize = true;
+                timeLable.Location = new Point(x, 70);
+
+                //pan.BorderStyle = BorderStyle.FixedSingle;
+
+                timeLable.TextAlign = ContentAlignment.TopRight;
+                timeLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                timeLable.ForeColor = Color.Black;
+                x += 150;
+                timeLable.Show();
+                paintPan.Controls.Add(timeLable);
+
+            }
+            }
+
+        private void Get_Schedule() //DB 에서 날짜에 해당하는 스케줄을 가져온다 
+        {
+            //string sql = "select * from SCHEDULE_TB";
+            //       sql += " where SC_UR_FK = 'U100000'"; 
+            //       sql += " and SC_STR_DT >= '2019-11-29'";
+            //       sql += " and SC_STR_DT < '2019-11-30'";
+            //        sql += "ORDER BY SC_STR_DT DESC";
+
+            //       db.ExecuteReader(sql); 
+
+            //  if(db.Reader.Read())
+            //  {
+
+            //  }
+        }
+        private void Get_Picture() //DB 에서 날짜에 해당하는 사진을 가져온다 
+        {
+            //string  sql = "select PIC_DATA from PICTURE_TB";
+            //        sql +=" where SC_UR_FK = 'U100000'"; 
+            //        sql += "and PIC_DT >= '2019-11-29'";
+            //        sql += " and PIC_DT < '2019-11-30'";
+
+            //db.AdapterOpen(sql);
+            //DataSet ds = new DataSet();
+            //db.Adapter.Fill(ds, "pictureTb");
+            //pictureTb = ds.Tables["pictureTb"];
+        }
+        private void Create_Picure(int location, Image img)
+        {
+            PictureBox pictureBox = new PictureBox();
+            panel2.Controls.Add(pictureBox); 
+
+    
+        }
+
+        private void 확인_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
