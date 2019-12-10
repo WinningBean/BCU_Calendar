2fff5ae92bb2d7e1ee7562f474d1d84affe177d7
jueun275 <52851721+jueun275@users.noreply.github.com>
2019-12-10 20:11:46 +0900

    Add DB Connectivity and Diary Write Functions

--- WindowsFormsApplication1/WindowsFormsApplication1/Diary.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Diary.Designer.cs
@@ -28,21 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.stateCheck = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 143);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(420, 319);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Diary";
+            // 
+            // date
+            // 
+            this.date.Font = new System.Drawing.Font("함초롬돋움", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.date.Location = new System.Drawing.Point(33, 106);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(243, 31);
+            this.date.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("함초롬돋움", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(213, 560);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "저장";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // stateCheck
+            // 
+            this.stateCheck.AutoSize = true;
+            this.stateCheck.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.stateCheck.Location = new System.Drawing.Point(132, 486);
+            this.stateCheck.Name = "stateCheck";
+            this.stateCheck.Size = new System.Drawing.Size(91, 30);
+            this.stateCheck.TabIndex = 15;
+            this.stateCheck.Text = "비공개";
+            this.stateCheck.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("함초롬돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label7.Location = new System.Drawing.Point(28, 486);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 26);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "공개상태";
             // 
             // Diary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(630, 635);
+            this.ClientSize = new System.Drawing.Size(514, 635);
+            this.Controls.Add(this.stateCheck);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Diary";
             this.Text = "Diary";
+            this.Load += new System.EventHandler(this.Diary_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox stateCheck;
+        private System.Windows.Forms.Label label7;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/Diary.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Diary.cs
@@ -10,11 +10,77 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
+
     public partial class Diary : Form
     {
+        DBConnection db = Program.DB;
+        private string diaryCD = null;
+
+        public Diary(DateTime nowDate)
+        {
+            InitializeComponent();
+            //this.nowDate = nowDate;
+        }
         public Diary()
         {
             InitializeComponent();
+
+        }
+        public DateTime EndDate
+        {
+            get { return date.Value; }
+            set { date.Value = value; }
+        }
+
+        public string DiaryCD
+        {
+            get { return diaryCD; }
+            set { diaryCD = value.ToString(); }
+        }
+
+        public int StateCheck
+        {
+            get { return Convert.ToInt32(stateCheck.Checked); }
+            set
+            {
+                int check = value;
+                if (check == 0)
+                {
+                    stateCheck.Checked = true;
+                }
+                else
+                {
+                    stateCheck.Checked = false;
+                }
+            }
+
+        }
+        // DateTime 
+
+        // DateTime nowDate = new DateTime();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string date = EndDate.ToString("yyyy/MM/dd 00:00");
+            string sql;
+            try
+            {
+                sql = "insert into DIARY_TB values('D'||to_char(seq_drcd.NEXTVAL),'" + textBox1.Text + "' , to_date('" + date + "', 'yyyy/MM/dd hh24:mi'),'" + db.UR_CD + "', null )";
+                db.ExecuteNonQuery(sql);
+                MessageBox.Show("일기를 등록했습니다", "완료", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show("데이터베이스 오류!! \n" + E.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            textBox1.Text = "";
+        }
+
+        private void Diary_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
