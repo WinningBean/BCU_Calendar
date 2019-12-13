e564f807ffcd0eb4189686dd14de4297bd77c1f9
wsb0722 <winningbean@gmail.com>
2019-12-14 00:23:19 +0900

    ADD GroupMember_Modify

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/GroupMember_Modify.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class GroupMember_Modify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Close_btn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Close_btn
+            // 
+            this.Close_btn.AutoSize = true;
+            this.Close_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Close_btn.Location = new System.Drawing.Point(308, 10);
+            this.Close_btn.Name = "Close_btn";
+            this.Close_btn.Size = new System.Drawing.Size(33, 25);
+            this.Close_btn.TabIndex = 3;
+            this.Close_btn.Text = "✖";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "그룹원 추가/삭제";
+            // 
+            // GroupMember_Modify
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(350, 350);
+            this.Controls.Add(this.Close_btn);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GroupMember_Modify";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "GroupMember_Modify";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Close_btn;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/GroupMember_Modify.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class GroupMember_Modify : Form
+    {
+        public GroupMember_Modify()
+        {
+            InitializeComponent();
+        }
+        public GroupMember_Modify(int is_Add = 0)
+        {
+            InitializeComponent();
+
+        }
+    }
+}
